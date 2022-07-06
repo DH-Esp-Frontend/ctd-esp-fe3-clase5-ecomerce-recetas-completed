7a4942b3b71a3b2aaf82e44d9754fc9f0d03224f
Alejo <alejoivanrojas@gmail.com>
2022-07-06 09:34:53 -0300

    test setup added

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,36 +1,38 @@
 import Router from 'next/router';
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form'
+
+interface IForm{
+  name: string,
+  image: string
+}
 
 const Create = () => {
-   const {register, handleSubmit, getValues} = useForm() 
+   const {register, handleSubmit, getValues, reset, formState: { errors }} = useForm<IForm>() 
 
-   const onSubmit = async()=> {
+   const onSubmit: SubmitHandler<IForm> = async()=> {
       const values = getValues()  
+      console.log(values);
       await fetch("http://localhost:3000/api/create", {
       method: "POST",
       body: JSON.stringify(values)
     })
+      reset()
       Router.push("/products")
     };
-   const onError = ()=> alert("Por favor, verifica la informacion") 
+   const onError: SubmitErrorHandler<IForm> = ()=> alert("Por favor, verifica la informacion") 
+
 
   return (
     <div>
         <form onSubmit={handleSubmit(onSubmit, onError)}>
-            <label>Title</label>
-            <input type="text" {...register('title')} />
-            <label>Name</label>
-            <input type="text" {...register('name')} />
-            <label>Author</label>
-            <input type="text" {...register('author')} />
-            <label>Publish date</label>
-            <input type="text" {...register('date')} />
-            <label>Price</label>
-            <input type="text" {...register('price')} />
-            <label>Image</label>
-            <input type="text" {...register('image')} />
-            
+            <label htmlFor="name" >Name</label>
+            <input id="name" type="text" {...register('name', { required: "Name is required", maxLength: 30 })} />
+            <p role="alert">{errors.name?.message}</p>
+
+            <label htmlFor="image">Image</label>
+            <input id='image' type="text" {...register('image', { required: true, pattern: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi })  } />
+            {errors.image && <p role="alert">Image is required</p>}
             <button type="submit" >submit</button>
         </form>
 
